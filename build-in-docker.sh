ed61d6dd253d124e866ca49016c0544d952450d0
Arnaud Morin <arnaud.morin@ovhcloud.com>
2025-03-12 16:03:34 +0100

    Build
    Signed-off-by: Arnaud Morin <arnaud.morin@ovhcloud.com>

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y build-essential debhelper dpkg-buildpackage
+apt-get install -y build-essential dpkg-dev devscripts
+
+cd $(dirname $(readlink -f $0))
 
 find * -maxdepth 0 -type d | while read folder ; do
     cd $folder
+    yes | mk-build-deps -i debian/control
     dpkg-buildpackage -b -uc
     cd ..
 done
