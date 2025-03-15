d4700bb28968d5190b3dd912cd1f6ec99be398f2
Arnaud M <arnaud.choupinette@mailops.fr>
2025-03-15 09:56:44 +0100

    Manage package version
    Signed-off-by: Arnaud M <arnaud.choupinette@mailops.fr>

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y build-essential dpkg-dev devscripts
+apt-get install -y build-essential dpkg-dev devscripts vim
 
-cd $(dirname $(readlink -f $0))
+DIR=$(dirname $(readlink -f $0))
+git config --global --add safe.directory $DIR
+cd $DIR
+
+# Compute version number for debian package based on number of commits up to this build
+# and the last git short sha
+C=$(git rev-list HEAD --count)
+S=$(git rev-parse --short HEAD)
+export DEBFULLNAME="Arnaud Morin"
+export DEBEMAIL="arnaud.openstack@mailops.fr"
 
 find * -maxdepth 0 -type d | while read folder ; do
     cd $folder
+    dch -b -v "2024.2-${C}+${S}" "New release"
     yes | mk-build-deps -i debian/control
     dpkg-buildpackage -b -uc
     cd ..
 done
+
+git clean -df
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v $PWD:/v/ debian:bookworm bash -c '/v/build-in-docker.sh'
+docker run -it --rm -v $PWD:/v/ debian:bookworm bash -c '/v/build-in-docker.sh'
