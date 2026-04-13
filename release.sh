8eb633b1a6bcf22f1b279fe61a748b229050aa36
Arnaud Morin <arnaud.morin@ovhcloud.com>
2026-04-13 21:14:59 +0000

    Update release script
    Signed-off-by: Arnaud Morin <arnaud.morin@ovhcloud.com>

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 VERSION=$(dpkg --info openstack-neutron*.deb | grep Version | awk '{print $2}')
-gh release create "v$VERSION" *.deb
+TARGET=$(git branch --show-current)
+echo "Will do a release v$VERSION with a tag on branch $TARGET"
+gh release create "v$VERSION" --target "$TARGET" *.deb
